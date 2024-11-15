30e4b96204acd276c2a9a20fc7cfcf79a5c65603
Giovanni Remigi <gremigi@theaiinstitute.com>
2024-11-15 01:02:53 +0100

    Using Agility Cassie as default scene.

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
         />
         <PerspectiveCamera makeDefault position={[2.0, 1.7, 1.7]} fov={45} />
         <OrbitControls makeDefault />
-        <Mujoco sceneUrl={"balloons/balloons.xml"} />
+        <Mujoco sceneUrl={"agility_cassie/scene.xml"} />
 
         {/* Post-Processing Effects */}
         <EffectComposer>
